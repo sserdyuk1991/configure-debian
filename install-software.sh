21dcff2479dcedbece108cd0867334e216951640
Serhiy Serdyuk <sserdyuk1991@gmail.com>
2017-09-11 00:12:36 +0300

    Update software installation script

--- install-software.sh
+++ install-software.sh
@@ -3,10 +3,13 @@
 PREFIX_DIR="software-installation-scripts"
 
 # Install core packages
-sudo apt-get -y install build-essential gdb vim tree incron git meld git-cola dos2unix ranger trash-cli htop keepass2 mono-complete libglu1-mesa-dev pdftk rxvt-unicode scrot gimp zathura openvpn xz-utils jmtpfs zip unzip xinput qbittorrent firefox-esr pcmanfm-qt network-manager network-manager-gnome
+sudo apt-get -y install build-essential gdb vim tree incron git meld git-cola dos2unix ranger trash-cli htop keepass2 mono-complete libglu1-mesa-dev pdftk rxvt-unicode scrot gimp zathura openvpn xz-utils jmtpfs zip unzip xinput qbittorrent firefox-esr pcmanfm-qt network-manager network-manager-gnome gedit file-roller bc galculator openssh-server tigervnc-standalone-server tigervnc-viewer tigervnc-common netcat-openbsd
 
 # Install additional packages
 sudo apt-get -y install clementine virtualbox-5.1
+
+# Update alternatives
+sudo update-alternatives --set editor /usr/bin/vim.basic
 
 # Install Awesome WM
 $PREFIX_DIR/install-awesome.sh
