5b827723b1ec51d81fa409c0a0f80d66ea6ac16a
Serhiy Serdyuk <sserdyuk1991@gmail.com>
2017-09-30 12:28:39 +0300

    Refine Qt installation script

--- software-installation-scripts/install-qt.sh
+++ software-installation-scripts/install-qt.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-wget -O /tmp/qt.run http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
-sudo chmod +x /tmp/qt.run
-sudo /tmp/qt.run
+INSTALLER="/tmp/qt.run"
+
+wget -O $INSTALLER http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
+sudo chmod +x $INSTALLER
+sudo $INSTALLER
