2d24d5c707953af561cb53469d8cbdf0016e2f5b
Serhiy Serdyuk <sserdyuk1991@gmail.com>
2017-09-30 19:32:17 +0300

    Fix configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-SRC_DIR="$HOME/tmp"
+SRC_DIR="$(dirname "$0")/.."
 
 # Install necessary drivers
 ./install-drivers.sh
@@ -14,7 +14,7 @@ sudo apt-get update && sudo apt-get -y -t *-backports upgrade
 ./install-software.sh
 
 # Restore backup files
-../debian-backup-files/backup-files/$HOME/.bin/restore-all.sh -s "$SRC_DIR"
+../debian-backup-files/backup-files$HOME/.bin/restore-all.sh -s "$SRC_DIR"
 
 # Reboot when configuring is complete
 sudo reboot
