501ce04d5788954afa5501eb38c9e97c748e2a80
Serhiy Serdyuk <sserdyuk1991@gmail.com>
2017-10-01 20:54:08 +0300

    Add couple of more applications to software list
    - upower
    - samba

--- install-software.sh
+++ install-software.sh
@@ -3,7 +3,7 @@
 PREFIX_DIR="software-installation-scripts"
 
 # Install core packages
-sudo apt-get -y install build-essential gdb vim tree incron git meld git-cola dos2unix ranger trash-cli htop keepass2 mono-complete libglu1-mesa-dev pdftk rxvt-unicode scrot gimp zathura openvpn xz-utils jmtpfs zip unzip xinput qbittorrent firefox-esr pcmanfm-qt network-manager network-manager-gnome gedit file-roller bc galculator openssh-server sshfs tigervnc-scraping-server tigervnc-viewer netcat-openbsd feh gparted acpid alsa-utils pavucontrol-qt libreoffice vinagre mpv
+sudo apt-get -y install build-essential gdb vim tree incron git meld git-cola dos2unix ranger trash-cli htop keepass2 mono-complete libglu1-mesa-dev pdftk rxvt-unicode scrot gimp zathura openvpn xz-utils jmtpfs zip unzip xinput qbittorrent firefox-esr pcmanfm-qt network-manager network-manager-gnome gedit file-roller bc galculator openssh-server sshfs tigervnc-scraping-server tigervnc-viewer netcat-openbsd feh gparted acpid alsa-utils pavucontrol-qt libreoffice vinagre mpv upower samba
 
 # Update alternatives
 sudo update-alternatives --set editor /usr/bin/vim.basic
