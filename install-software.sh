80b06125f84c26b919ebf36f400961a96bc6f493
Serhiy Serdyuk <sserdyuk1991@gmail.com>
2017-10-21 00:39:22 +0300

    Add debootstrap to software installation script

--- install-software.sh
+++ install-software.sh
@@ -5,7 +5,7 @@ set -e
 PREFIX_DIR="software-installation-scripts"
 
 # Install core packages
-sudo apt-get -y install build-essential gdb vim tree incron git meld git-cola dos2unix ranger trash-cli htop keepass2 mono-complete libglu1-mesa-dev pdftk rxvt-unicode scrot gimp zathura openvpn xz-utils jmtpfs zip unzip xinput qbittorrent firefox-esr pcmanfm-qt network-manager network-manager-gnome gedit file-roller bc galculator openssh-server sshfs tigervnc-scraping-server netcat-openbsd feh gparted acpid alsa-utils pavucontrol-qt libreoffice vinagre mpv upower samba systemd-container elinks
+sudo apt-get -y install build-essential gdb vim tree incron git meld git-cola dos2unix ranger trash-cli htop keepass2 mono-complete libglu1-mesa-dev pdftk rxvt-unicode scrot gimp zathura openvpn xz-utils jmtpfs zip unzip xinput qbittorrent firefox-esr pcmanfm-qt network-manager network-manager-gnome gedit file-roller bc galculator openssh-server sshfs tigervnc-scraping-server netcat-openbsd feh gparted acpid alsa-utils pavucontrol-qt libreoffice vinagre mpv upower samba debootstrap systemd-container elinks
 
 # Update alternatives
 sudo update-alternatives --set editor /usr/bin/vim.basic
