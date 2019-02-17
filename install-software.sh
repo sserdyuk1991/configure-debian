c9f4fdbf58987b1f8c85864f97eb5a5b6e887ce9
Serhiy Serdyuk <sserdyuk1991@gmail.com>
2019-02-18 00:29:35 +0200

    Update install-software.sh
    - emacs25
    - info
    + linux-headers-amd64
    + cmake
    + curl
    + acl
    + arandr
    + pdf-presenter-console
    + xautolock
    + brasero
    + pmount

--- install-software.sh
+++ install-software.sh
@@ -5,7 +5,7 @@ set -e
 PREFIX_DIR="software-installation-scripts"
 
 # Install core packages
-sudo apt-get -y install build-essential gdb vim tree incron git meld git-cola dos2unix ranger trash-cli htop keepass2 mono-complete libglu1-mesa-dev pdftk rxvt-unicode scrot gimp zathura openvpn xz-utils jmtpfs zip unzip xinput qbittorrent firefox-esr pcmanfm-qt network-manager network-manager-gnome gedit file-roller bc galculator openssh-server sshfs tigervnc-scraping-server netcat-openbsd feh gparted acpid alsa-utils pavucontrol-qt libreoffice vinagre mpv upower samba debootstrap systemd-container elinks emacs25 info
+sudo apt -y install linux-headers-amd64 build-essential cmake gdb vim tree incron git meld git-cola dos2unix ranger trash-cli htop keepass2 mono-complete libglu1-mesa-dev pdftk rxvt-unicode scrot gimp zathura openvpn xz-utils jmtpfs zip unzip xinput qbittorrent firefox-esr pcmanfm-qt network-manager network-manager-gnome gedit file-roller bc galculator openssh-server sshfs tigervnc-scraping-server netcat-openbsd curl feh gparted acpid alsa-utils pavucontrol-qt libreoffice vinagre mpv upower samba debootstrap systemd-container elinks acl arandr pdf-presenter-console xautolock brasero pmount
 
 # Update alternatives
 sudo update-alternatives --set editor /usr/bin/vim.basic
@@ -13,8 +13,8 @@ sudo update-alternatives --set editor /usr/bin/vim.basic
 # Install Awesome WM
 $PREFIX_DIR/install-awesome.sh
 
-# Install Slack
-$PREFIX_DIR/install-slack.sh
+# Install Spacemacs
+$PREFIX_DIR/install-spacemacs.sh
 
-# Install Viber
-$PREFIX_DIR/install-viber.sh
+# Install Syncthing
+$PREFIX_DIR/install-syncthing.sh
